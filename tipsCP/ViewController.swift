b1104deda7d63dc287709b01a24d3aa31ef55f62
Oliver Thurn <oliverthurn@olivers-macbook-pro-2.local>
2019-01-31 23:06:22 -0800

    Initial Commit

---
+++ tipsCP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipsCP
+//
+//  Created by Oliver Thurn on 1/31/19.
+//  Copyright © 2019 Oliver Thurn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
